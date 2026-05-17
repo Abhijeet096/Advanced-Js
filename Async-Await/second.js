59e77bd3eb6b34f0ff24b5458f79f343a2e6f0ac
Abhijit <tradeabhiyt@gmail.com>
2026-05-17 12:02:37 +0530

    adding IIFE functioction file

--- Async-Await/second.js
+++ Async-Await/second.js
@@ -23,5 +23,20 @@ async function getAllData(){
     
 }
 
+
+// iife funtions --> no need to named or call them they executed imidiately where they get defined
+
+// (async function (){
+//       console.log("Getting data 1.....")
+//    let p1 = await getData(1);
+//    console.log(p1);
+//     console.log("Getting data 1.....")
+//    let p2 = await getData(2);
+//    console.log(p2);
+//     console.log("Getting data 1.....")
+//    let p3 = await getData(3);
+//    console.log(p3);
+// }) ();
+
 getAllData();
 
